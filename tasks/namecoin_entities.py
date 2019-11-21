eed8fe12247f53f90231fb1e0e4be7be3a67d53a
Andrey I. Skhomenko <a.skhomenko@norsi-trans.ru>
2019-11-21 18:59:23 +0300

    helpers for enterparams and other

--- tasks/namecoin_entities.py
+++ tasks/namecoin_entities.py
@@ -3,3 +3,12 @@ __author__ = 'sai'
 NamecoinDNS = "sai.NamecoinName"
 NamecoinAddress = "sai.Address"
 NamecoinTX = "sai.TX"
+
+keys_for_parse_enterparams = {"IPtoNamecoin": {"ipv4-address": 0},
+                              "NamecointoIP": {"fqdn": 0},
+                              "NetblocktoIP": {"ipv4-range": 0},
+                              "ASNIPtoBlock": {"ipv4-address": 0}}
+
+
+ip_blocked_list = ["127.0.0.1"]
+# TODO need dev func. for check private or not ip address - easily
